018709018b27a035e6e6b192461b626843538968
Vivekananda Shet <vivekshet1495@gmail.com>
2025-09-16 23:13:09 +0530

    Update Microservice1Application.java

--- src/main/java/com/example/microservice1/Microservice1Application.java
+++ src/main/java/com/example/microservice1/Microservice1Application.java
@@ -2,6 +2,8 @@ package com.example.microservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.InetAddress;
@@ -9,7 +11,13 @@ import java.net.UnknownHostException;
 
 @SpringBootApplication
 @RestController
-public class Microservice1Application {
+public class Microservice1Application extends SpringBootServletInitializer {
+
+    // Needed for WAR deployment in Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Microservice1Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Microservice1Application.class, args);
